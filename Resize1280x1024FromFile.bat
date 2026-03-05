fc9d595c5da6e942a27b35d54e3f49b6743f76f2
Paulo-Pinheiro <93837893+Paulo-Pinheiro@users.noreply.github.com>
2026-03-05 11:30:42 +0000

    Update Resize1280x1024FromFile.bat

--- Resize1280x1024FromFile.bat
+++ Resize1280x1024FromFile.bat
@@ -15,7 +15,7 @@ FOR /F %%i IN (VerticalGames.txt) DO (
 
 	IF EXIST %%i.png (
 
-		magick %%i.png -resize 1280x1024\! -crop 2x1@ -resize 426x1024\! %%i.png
+        magick %%~x.png -filter Lanczos -resize 852x1024\! -crop 2x1@ %%~x.png
 		magick convert -size 428x1024 xc:transparent transparent.png
 		magick montage -mode concatenate -tile 3x %%i-0.png transparent.png %%i-1.png -background none %%i_1280x1024.png
 
@@ -37,3 +37,4 @@ echo Number of processed files %argCount%
 echo Number of written files   %argCountGood%
 echo Number of failed files    %argCountBad%
 echo Done!
+
